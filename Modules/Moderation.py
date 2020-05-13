1bca0c78eb4bba1d062efc52073315bf34f94a91
Sebastian Kramm <sebastian.kramm@protonmail.com>
2020-05-13 18:34:12 +0200

    added custom prefix

--- Modules/Moderation.py
+++ Modules/Moderation.py
@@ -1,11 +1,21 @@
 from discord.ext import commands
 import typing
+import discord
+import mops
 
 class Moderation(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: mops.MopsClient):
         self.bot = bot
         print("moderation init")
 
     @commands.command(help = "it pongs")
     async def ping(self, ctx):
         await ctx.send('pong')
+
+    @commands.command(help = "Changes the prefix of Mops in the current Guild")
+    @commands.has_permissions(manage_channel=True)
+    async def SetPrefix(self, ctx: commands.Context, prefix):
+        ctx.channel().trigger_typing()
+        oldPrefix = self.bot.get_prefix(ctx.guild())
+        self.bot.insert_or_update_prefix(ctx.guild(), prefix)
+        ctx.send("Changed prefix from `{0}` to `{1}".format(oldPrefix, prefix))
--- mops.py
+++ mops.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
-import commandHandler
 from TrackerClient import TrackerClient
 import threading
 import json
 import time
 import asyncio
 import pymongo
+import pkgutil
+import sys
+import inspect
 
 class MopsClient(commands.AutoShardedBot):
     def __init__(self, config, **kwargs):
@@ -14,7 +16,14 @@ class MopsClient(commands.AutoShardedBot):
         self.shards_ready = 0
         self.databaseClient = pymongo.MongoClient(config["DatabaseURL"])
         self.database = self.databaseClient["Mops"]
-        super(MopsClient, self).__init__(**kwargs)
+
+        self.prefixList = dict()
+        for doc in self.database["GuildPrefixes"]:
+            self.prefixList[doc["_id"]] = doc["Value"]
+
+        self.loadModules()
+
+        super(MopsClient, self).__init__(**kwargs, command_prefix=prefix)
 
 
     async def on_shard_ready(self, shard_id):
@@ -26,14 +35,37 @@ class MopsClient(commands.AutoShardedBot):
             tracker_client = TrackerClient(client, "127.0.0.1", 11000)
             asyncio.ensure_future(tracker_client.start_connection_loop())
 
+    def get_prefix(self, guild):
+        if guild.id in self.prefixList:
+            return self.prefixList[guild.id]
+        else:
+            return '!'
+
+    def insert_or_update_prefix(self, guild: discord.Guild, prefix: str):
+        self.prefixList[guild.id] = prefix
+        self.database["GuildPrefixes"].update_one({'_id':guild.id},{"_id": guild.id, "Value": prefix}, True)
+
+    def loadModules(self):
+        if("Modules" not in sys.modules):
+            pkgutil.find_loader("Modules").load_module("Modules")
+        for importer, package_name, _ in pkgutil.iter_modules(["Modules"]):
+            full_package_name = '%s.%s' % ("Modules", package_name)
+            if full_package_name not in sys.modules:
+                    module = importer.find_module(full_package_name
+                                ).load_module(full_package_name)
+                    for name,obj in inspect.getmembers(module):
+                        if inspect.isclass(obj):
+                            if(issubclass(obj,commands.Cog)):
+                                self.add_cog(obj(self))
+    
+def prefix(bot, message:discord.Message):
+    return commands.when_mentioned_or(bot.get_prefix(message.guild()))(bot, message)
 
 if __name__ == "__main__":
     with open("./mopsdata/Config.json", mode="r") as file:
         data = file.read()
     config = json.loads(data)
 
-    client = MopsClient(config, fetch_offline_members=False, guild_subscriptions=False, command_prefix='$')
-    client.add_cog(commandHandler.commandHandler(client))
-
+    client = MopsClient(config, fetch_offline_members=False, guild_subscriptions=False)
 
     client.run(config["Discord"])
