a857e8dfe0c7436996d888d792edb01c6d14e5fe
Marilyth <baermay98@gmail.com>
2020-05-13 11:13:35 +0200

    Make things work, kinda

---
+++ TrackerClient.py
@@ -0,0 +1,62 @@
+import discord
+import socket
+import json
+import time
+import asyncio
+from dateutil import parser
+
+class TrackerClient:
+    def __init__(self, bot, ip: str, port: int):
+        self.HOST = '127.0.0.1'
+        self.PORT = 11000
+        self.server: socket = None
+        self.bot = bot
+
+    async def send_message(self, message: str):
+        self.server.send(str.encode(message))
+
+    async def received_message(self, message: str):
+        try:
+            print("data recieved: {0}".format(message))
+
+            data = message.split("STARTEVENT ")[1].split(sep="\n", maxsplit=1)
+            event_id = int(data[0])
+            eventMessage = json.loads(data[1].split("ENDEVENT " + str(event_id))[0])
+
+            channel = self.bot.get_channel(eventMessage["ChannelId"])
+            embed = self.dnet_embed_to_py(eventMessage["Embed"])
+
+            await channel.send(eventMessage["Notification"], embed=embed)
+        except:
+            pass
+
+    async def start_connection_loop(self):
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                try:
+                    self.server = s
+                    self.server.connect((self.HOST, self.PORT))
+                    while True:
+                            data = self.server.recv(8192)
+                            await self.received_message(data.decode())
+                            await self.send_message("Received data")
+                except Exception as e:
+                    print(e)
+                    print("Connection was interrupted, trying again in 10 seconds.")
+                    asyncio.sleep(10)
+
+    def dnet_embed_to_py(self, json_dict) -> discord.Embed:
+        embed = discord.Embed()
+        embed.title = json_dict["Title"]
+        embed.description = json_dict["Description"]
+        embed.url = json_dict["Url"]
+        embed.timestamp = parser.parse(json_dict["Timestamp"])
+        embed.colour = discord.colour.Colour(json_dict["Color"]["RawValue"])
+        embed.set_image(url=json_dict["Image"]["Url"])
+        embed.set_author(name=json_dict["Author"]["Name"], url=json_dict["Author"]["Url"], icon_url=json_dict["Author"]["IconUrl"])
+        embed.set_footer(text=json_dict["Footer"]["Text"], icon_url=json_dict["Footer"]["IconUrl"])
+        embed.set_thumbnail(url=json_dict["Thumbnail"]["Url"])
+        #ToDo: Handle fields
+
+        return embed
+    
--- mops.py
+++ mops.py
@@ -1,13 +1,17 @@
 import discord
 import commandHandler
-from trackerHandler import startTrackerHandler
+from TrackerClient import TrackerClient
 import threading
 import json
+import time
+import asyncio
 
 class MopsClient(discord.AutoShardedClient):
-    def __init__(self, **kwargs):
+    def __init__(self, config, **kwargs):
         self.messageHandler = []
         self.reactionHandler = []
+        self.config = config
+        self.shards_ready = 0
         super(MopsClient, self).__init__(**kwargs)
 
     def addMessageHandler(self, handler):
@@ -18,6 +22,12 @@ class MopsClient(discord.AutoShardedClient):
 
     async def on_shard_ready(self, shard_id):
         print('Mopsbot ready shard id {0} total shards: {1} ready'.format(shard_id, self.shard_count))
+
+        self.shards_ready += 1
+        if self.shards_ready == self.shard_count:
+            print("All shards ready, starting tracker client")
+            tracker_client = TrackerClient(client, "127.0.0.1", 11000)
+            asyncio.ensure_future(tracker_client.start_connection_loop())
 
     async def on_message(self, message):
         for handler in self.messageHandler:
@@ -31,14 +41,13 @@ class MopsClient(discord.AutoShardedClient):
         for handler in self.messageHandler:
             await handler.on_reaction_remove(self, reaction, user)
 
-with open("./mopsdata/Config.json", mode="r") as file:
-    data = file.read()
-config = json.loads(data)
 
-client = MopsClient(fetch_offline_members=False, guild_subscriptions=False)
-client.addMessageHandler(commandHandler.commandHandler())
+if __name__ == "__main__":
+    with open("./mopsdata/Config.json", mode="r") as file:
+        data = file.read()
+    config = json.loads(data)
 
-trackerHandlerThread = threading.Thread(target = startTrackerHandler)
-trackerHandlerThread.start()
+    client = MopsClient(config, fetch_offline_members=False, guild_subscriptions=False)
+    client.addMessageHandler(commandHandler.commandHandler())
 
-client.run(config["Discord"])
+    client.run(config["Discord"])
